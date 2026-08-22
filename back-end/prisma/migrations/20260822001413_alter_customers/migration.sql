08b2c5dbdfaf9fd5cb9d906c3bc3f0da8d414d54
Laura Arantes <122477377+Lalamarantes@users.noreply.github.com>
2026-08-22 01:07:56 +0000

    (21/08) Configuração do Prisma Client e primeiro repository

---
+++ back-end/prisma/migrations/20260822001413_alter_customers/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[ident_document]` on the table `Customer` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- CreateIndex
+CREATE UNIQUE INDEX "Customer_ident_document_key" ON "Customer"("ident_document");
